7185871fcf6f2e5df37a406667fcbbb919bf9729
Ed Sweeney <ed@onextent.com>
2020-07-19 20:23:29 -0700

    fix jwt unit test

--- src/test/scala/somind/auth/JwtSpec.scala
+++ src/test/scala/somind/auth/JwtSpec.scala
@@ -5,12 +5,13 @@ import java.security.interfaces.RSAPublicKey
 import com.auth0.jwk.UrlJwkProvider
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.auth0.jwt.interfaces.DecodedJWT
+import com.auth0.jwt.exceptions.TokenExpiredException
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 import somind.auth.verifyproxy.Conf._
 
-class JwtSpec extends AnyFlatSpec with should.Matchers {
+class JwtSpec extends AnyFlatSpec with should.Matchers with LazyLogging {
 
   "A jwt" should "parse" in {
 
@@ -29,8 +30,16 @@ class JwtSpec extends AnyFlatSpec with should.Matchers {
       Algorithm.RSA256(jwk.getPublicKey.asInstanceOf[RSAPublicKey], null)
     val verifier = JWT.require(algorithm).build()
 
-    val result: DecodedJWT = verifier.verify(token)
-    println(result)
+    try {
+
+    verifier.verify(token)
+      assert(false, "no exception")
+    } catch {
+      case e: TokenExpiredException =>
+        logger.debug("good, token is expired")
+      case e: Throwable =>
+        assert(false, s"wrong exception: $e")
+    }
 
   }
 
